aaa4d3747e4a1e6f64e528f824b218d48c9f03a0
Yuri <yuri.vasquez91@gmail.com>
2015-09-09 03:26:59 -0300

    fatboy is a class now

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-module.exports = function (req) {
-  var modules = {};
-  function load_modules() {
-    // console.log(this);
-    Object.keys(modules).forEach(function (name) {
-      m = modules[name];
-      this[name] = req(m);
+function FatBoy(req) {
+  this.req = req;
+}
 
-    }.bind(this));
-  }
-  var public = {
-    add:function(module, name) {
-      name = name || module;
-      modules[name] = module;
+FatBoy.prototype = {
+  modules: undefined,
+  load:function (context) {
+    if (this.modules) {
+      context = context || {};
+      var fatboy = this;
+      Object.keys(this.modules).forEach(function (alias) {
+        m = fatboy.modules[alias];
+        context[alias] = fatboy.req(m);
+
+      }.bind(context));
+      return context;
+    }
+  },
+  add: function(module_name, alias) {
+      this.modules = this.modules || {};
+      alias = alias || module_name;
+      this.modules[alias] = module_name;
 
       return this;
-    },
-    load:function (context) {
-      load_modules.bind(context)();
     }
-  };
-  return public;
 };
+
+module.exports = FatBoy;
